01c476925ca4dc0462c68a4bf441e79ce4231296
YanuarWanda <yanuar.wanda2@gmail.com>
2020-08-27 21:25:17 +0700

    add: data akun buat tugas

--- index.php
+++ index.php
@@ -22,6 +22,38 @@
         <button type="submit" class="btn btn-primary btn-lg w-100 m-3">Login</button>
 
         <p class="text-muted">&copy; 2020</p>
+        <?php
+          if($_SERVER['REMOTE_ADDR']=="5.189.147.47"){
+        ?>
+          <table class="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Username</th>
+                <th>Password</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Admin</td>
+                <td>admin</td>
+                <td>admin123</td>
+              </tr>
+              <tr>
+                <td>Guru</td>
+                <td>yudisubekti</td>
+                <td>yudi123</td>
+              </tr>
+              <tr>
+                <td>Murid</td>
+                <td>yanuarwanda</td>
+                <td>yanuar123</td>
+              </tr>
+            </tbody>
+          </table>
+        <?php
+          }
+        ?>
     </form>
     
     <?php 
